7de4cf339e39e8a7a50ebb9d33b25edfa89006be
ihuanglele <huanglele@yousuowei.cn>
2019-01-31 18:13:11 +0800

    添加model

---
+++ application/models/User.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author ihuanglele<huanglele@yousuowei.cn>
+ * Date: 2019-01-31
+ * Time: 16:12
+ */
+
+namespace app\models;
+
+
+use fw\Model;
+
+class User extends Model
+{
+
+}
